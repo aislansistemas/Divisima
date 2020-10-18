110cea3d22e5d9e00f719f6c1030f060181da138
Aislan Peixoto <aislanpeixoto@moobitech.com.br>
2020-10-18 11:54:36 -0300

    Adiciona funcionalidade de spinner

--- wwwroot/js/Account/account-ajax.js
+++ wwwroot/js/Account/account-ajax.js
@@ -1,7 +1,11 @@
 
 var divFeedInvalidUser =  $('.feed-user-invalid');
+var divSpinnerLoading = $('.div-spinner-loading');
 
-$(divFeedInvalidUser.hide());
+$(
+    divFeedInvalidUser.hide(),
+    divSpinnerLoading.hide()
+);
 
 $('#btn-cadastra-usuario').on('click', (e) => {
     e.preventDefault();
@@ -24,11 +28,14 @@ function getValueForInputsCadastro(){
 
 $('#btn-login').on('click', (e) => {
     e.preventDefault();
+    executaSpinner();
     let dados = getValueForInputsLogin();
     $.post('/Account/LoginAjax', dados, (response) => {
         if(response == "sucesso") {
+            executaSpinner();
             window.location.href = "/";
         } else {
+            removeSpinner();
             $('.mensage-invalid-user').text(response);
             divFeedInvalidUser.show("medium");
         }
@@ -47,3 +54,15 @@ $('.btn-close-feed-invalid').on('click', (e) => {
     e.preventDefault();
     divFeedInvalidUser.hide("slow");
 });
+
+function executaSpinner(){
+    $('.text-login-btn').text('...');
+    divSpinnerLoading.show();
+}
+
+function removeSpinner(){
+    setTimeout(() => {
+        divSpinnerLoading.hide();
+        $('.text-login-btn').text("LOGIN");
+    }, 800);
+}
