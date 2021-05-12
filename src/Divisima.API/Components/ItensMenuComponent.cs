d69e11780a4590be15ff242c01db5146af483e5e
aislansistemas <aislanhouse10@gmail.com>
2021-05-12 19:59:24 -0300

    Aplicando testes unitarios

---
+++ src/Divisima.API/Components/ItensMenuComponent.cs
@@ -0,0 +1,24 @@
+﻿using divisima.Repository.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Divisima.API.Components
+{
+    public class ItensMenuComponent : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        public ItensMenuComponent(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.GetAllAtiva().Result;
+            return View(categorias);
+        }
+    }
+}
---
+++ src/Divisima.Tests/RepositoryTest/ProdutoRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using divisima.Enums.ProdutoEnums;
+using divisima.Models;
+using divisima.Repository.Contracts;
+using Divisima.Enums.GeneroEnum;
+using Divisima.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Divisima.Tests.RepositoryTest
+{
+    public class ProdutoRepositoryTest
+    {
+        [Fact(DisplayName = "Testar método obter um produto por id")]
+        public async Task Obter_Produto_Por_Id_Success_Async()
+        {
+            var produto = new Produto
+            {
+                ProdutoId = 1,
+                CategoriaId = 1,
+                Nome = Faker.Company.Name(),
+                Descricao = "Alguma Descricao",
+                Valor = Faker.RandomNumber.Next(),
+                Quantidade = Faker.RandomNumber.Next(),
+                Tamanho = (int)ProdutoTamanhoEnum.P,
+                DataCadastro = Faker.DateOfBirth.Next(),
+                Genero = GeneroEnum.Masculino           
+            };
+
+            var produtoRepositoryMock = new Mock<IProdutoRepository>();
+            produtoRepositoryMock.Setup(p => p.GetById(produto.ProdutoId)).ReturnsAsync(produto);
+            var produtoRepository = produtoRepositoryMock.Object;
+
+            var produtoResult = await produtoRepository.GetById(produto.ProdutoId);
+            Assert.NotNull(produtoResult);
+            Assert.True(produtoResult.ProdutoId == produto.ProdutoId);
+            Assert.Equal(produtoResult.Nome, produto.Nome);
+
+            produtoRepositoryMock = new Mock<IProdutoRepository>();
+            produtoRepositoryMock.Setup(p => p.GetById(It.IsAny<long>())).ReturnsAsync((Produto) null);
+            produtoRepository = produtoRepositoryMock.Object;
+
+            var produtoResultNotFound = await produtoRepository.GetById(produto.ProdutoId);
+            Assert.Null(produtoResultNotFound);
+        }
+    }
+}
